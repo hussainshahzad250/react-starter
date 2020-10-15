bba85530868796bf5af62b1b24e053c2cd90cb8e
Shahzad Hussain <hussain.shahzad250@gmail.com>
2020-10-16 01:13:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import Home from "./components/Home";
 import About from "./components/About";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { Contact } from "./components/Contact";
-import { NewHome } from "./components/NewHome";
 
 function App() {
   return (
     <div className="App">
      
      <BrowserRouter>
-      {/* <Route exact path="/" component={Home}/>
+      <Route exact path="/" component={Home}/>
       <Route exact path="/about" component={About}/>
-      <Route exact path="/contact" component={Contact}/> */}
-      <Route exact path="/" component={NewHome}/>
+      <Route exact path="/contact" component={Contact}/>
+      
      
      
      </BrowserRouter>
